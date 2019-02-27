21da6e7b758b39cfb6063884a1ef68e877c6cc1a
alessiogilardi <36900296+alessiogilardi@users.noreply.github.com>
2019-02-27 18:03:13 +0100

    Update

---
+++ src/Construct.java
@@ -0,0 +1,115 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.Random;
+
+public class Construct {
+
+    public static ArrayList<Edge> priorityConstructGraph(ArrayList<Degree> newDist, ArrayList<Edge> origGraph) {
+        ArrayList<Edge> edges = new ArrayList<>();
+        int sum = 0;
+        for (Degree d : newDist) { sum += d.getDegree(); }
+
+        if (sum % 2 != 0) {
+//			System.out.println("infattibile " + sum);
+            return edges;
+        }
+
+        for (Degree selected : newDist) {
+            String w;
+            while (selected.getDegree() > 0) {
+                w = null;
+                for (Edge edge : origGraph) {
+                    if (edge.getPrev().equals(selected.getName())) {
+                        w = edge.getNext();
+                        origGraph.remove(edge);
+                        break;
+                    }
+                }
+
+                if (w == null) { break; }
+
+                Edge e1 = new Edge(selected.getName(), w);
+                edges.add(e1);
+                for (Degree d : newDist) {
+                    if (d.getName().equals(selected.getName()) ) {
+                        d.setDegree(d.getDegree() - 1);
+                    }
+                }
+            }
+        }
+
+        Collections.sort(newDist);
+
+        for (int i=0; i < newDist.size(); i++) {
+            for (int j=0; j < newDist.size(); j++) {
+                if (i != j) {
+                    if (newDist.get(i).getDegree() > 0 && newDist.get(j).getDegree() > 0) {
+                        Edge e1 = new Edge(newDist.get(i).getName(), newDist.get(j).getName());
+                        Edge e2 = new Edge(newDist.get(j).getName(), newDist.get(i).getName());
+
+                        if (!edges.contains(e1) && !edges.contains(e2)) {
+                            edges.add(e1);
+                            edges.add(e2);
+                            newDist.get(i).setDegree(newDist.get(i).getDegree() - 1);
+                            newDist.get(j).setDegree(newDist.get(j).getDegree() - 1);
+                            //System.out.println("+++ " + e1.v + " " + e1.w);
+                        }
+                    }
+                }
+            }
+        }
+        return edges;
+    }
+
+    public static ArrayList<Edge> greedyConstructGraph(ArrayList<Degree> newDist) throws InterruptedException {
+//		System.out.println("anonimizzazione grafo con l'algoritmo greedy");
+        ArrayList<Edge> edges = new ArrayList<>();
+        int sum = 0;
+        for(Degree d : newDist) { sum += d.getDegree(); }
+
+        if(sum%2 != 0) {
+            return edges;
+//			System.out.println("infattibile " + sum);
+        }
+
+//		System.out.println("fattibile " + sum);
+        while(sum > 0) {
+            sum = 0;
+            Random random = new Random();
+            int k = random.nextInt(newDist.size());
+            Degree v = newDist.get(k);
+            //grado del nodo preso randomicamente da newdist
+            if (v.getDegree() > 0) {
+                //setto come max di riferimento il grado del nodo preso randomicamente
+                int max = v.getDegree();
+                //ne setto ora il grado a 0
+                v.setDegree(0);
+                //ordino newdist per grado in maniera tale da prendere quelli di grado pi˘ alto succedssivaemnte nel codice
+                Collections.sort(newDist);
+
+
+                for (int i=0; i<max; i++) {
+                    //fino al grado del nodo prelevato randomicamente
+                    //prelevo da newdist il primo nodo ogni volta ( grado piu alto)
+                    //e lo linko a quello prelevato radnomicamente
+                    if (!newDist.get(i).getName().equals(v.getName())) {
+                        Edge e1 = new Edge(v.getName(), newDist.get(i).getName());
+                        Edge e2 = new Edge(newDist.get(i).getName(), v.getName());
+                        newDist.get(i).setDegree(newDist.get(i).getDegree()-1);
+                        edges.add(e1);
+                        edges.add(e2);
+                    }
+                }
+            }
+
+            // Calcolo la somma dei gradi
+            for(Degree d : newDist) { sum += d.getDegree(); }
+        }
+        return edges;
+    }
+
+
+
+
+}
--- src/Main.java
+++ src/Main.java
@@ -1,38 +1,124 @@
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Scanner;
+import java.io.*;
+import java.util.*;
 
 
 public class Main {
-    public static String fileIn;
-    public static void main(String[] args) throws FileNotFoundException {
-        fileIn = args[0];
+    //public static String fileIn;
+    private static String[] ANON_ALGOS = new String[]{"greedy", "dp"};
+    private static String[] CONSTR_ALGOS = new String[]{"constr", "prio"};
+
+    public static void main(String[] args) throws FileNotFoundException, InterruptedException, IOException {
+        String fileIn = args[0];
         int k = Integer.parseInt(args[1]);
         String anonMode = args[2];
         String buildMode = args[3];
 
         ArrayList<ArrayList<String>> graph = LoadData.readData(fileIn);
-        ArrayList<Degree>   dist        = LoadData.getDegrees(graph);
-        ArrayList<Edge>     origEdges   = LoadData.getEdges(graph);
+
+        ArrayList<Degree> dist = LoadData.getDegrees(graph);
+        ArrayList<Edge> origGraph = LoadData.getEdges(graph);
 
         ArrayList<DA> das = new ArrayList<>();
-        ArrayList<Degree> grouped;
+        ArrayList<Degree> grouped = null;
 
-        if (anonMode.equals("greedy")) {
+        if (anonMode.equals(ANON_ALGOS[0])) {
             // Creazione lista dei gradi con algoritmo: greedy
             grouped = Grouper.greedyGrouper(k, dist);
         }
-        if (anonMode.equals("dp")) {
+        if (anonMode.equals(ANON_ALGOS[1])) {
             // Creazione lista dei gradi con algoritmo: dynamic programming
             grouped = Grouper.dpGrouper(k, dist, das);
         }
 
+        int initcost = 0;
+        for (Degree d : dist) {
+            initcost += d.getDegree();
+        }
 
+        int anoncost = 0;
+        for (Degree d : grouped) {
+            anoncost += d.getDegree();
+        }
+
+        long startTime = System.currentTimeMillis();
+
+        ArrayList<Edge> edges = null;
+        if (buildMode.equals(CONSTR_ALGOS[0])) {
+            //Anonimizza con l'algoritmo greedy
+            edges = Construct.greedyConstructGraph(grouped);
+        }
+
+        if (buildMode.equals(CONSTR_ALGOS[1])) {
+            //Anonimizza con l'algoritmo Priority
+            edges = Construct.priorityConstructGraph(grouped, origGraph);
+        }
+        
+        long finishTime = System.currentTimeMillis();
+        long totalTime = finishTime - startTime;
+
+        printAnonymizedGraph(edges, k, fileIn);
+
+        int diversity = 0;
+        for (Edge e : origGraph) {
+            if (!edges.contains(e)) {
+                diversity++;
+            }
+        }
+
+        int cost = 0;
+
+        if (anonMode.equals(ANON_ALGOS[0])) {
+            cost = initcost - anoncost;
+        }
+
+        if (anonMode.equals(ANON_ALGOS[1])) {
+            cost = das.get(das.size() - 1).getCost();
+        }
+
+        printCostToCSV(fileIn, k, anonMode, buildMode, cost, totalTime);
     }
 
 
+    private static void printCostToCSV(String fileIn, int k, String anonMode, String buildMode, int cost, double time) throws IOException {
+        FileWriter fw = new FileWriter("output.csv", true);
+        PrintWriter pw = new PrintWriter(fw);
+        String output = fileIn + "; " + k + "; " + anonMode + "; " + buildMode + "; " + cost + "; " + time;
+
+        pw.write(output);
+        pw.println();
+        pw.close();
+    }
 
 
+    private static void printAnonymizedGraph(ArrayList<Edge> edges, int k, String fileIn) throws IOException {
+        if (edges.isEmpty()) {
+            return;
+        }
+
+        Collections.sort(edges, new Comparator<Edge>() {
+            @Override
+            public int compare(Edge edge1, Edge edge2) {
+                return edge1.getPrev().compareTo(edge2.getPrev());
+            }
+        });
+
+        FileWriter fw   = new FileWriter("Anon_" + k + "_" + fileIn, false);
+        PrintWriter pw  = new PrintWriter(fw);
+
+        int idx = 0;
+        while (idx < edges.size()) {
+            StringBuilder sb    = new StringBuilder();
+            String first        = edges.get(idx).getPrev();
+            sb.append(first);
+            sb.append(';');
+            while (idx < edges.size() && first.equals(edges.get(idx).getPrev())) {
+                sb.append(edges.get(idx).getNext());
+                sb.append(';');
+                idx++;
+            }
+            pw.write(sb.toString());
+            pw.println();
+        }
+        pw.close();
+    }
 }
