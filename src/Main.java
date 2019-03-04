60bb63c840a0a9c9737ad64489546be0853f82a6
iPaul1z3r <36900808+iPaul1z3r@users.noreply.github.com>
2019-03-04 12:30:50 +0100

    update del sorgente e degli script

--- src/Main.java
+++ src/Main.java
@@ -6,20 +6,21 @@ import java.util.Collections;
 
 import objects.*;
 
+
 public class Main {
-    private static final String[] ANON_ALGOS      = new String[]{"greedy", "dp"};
-    private static final String[] CONSTR_ALGOS    = new String[]{"constr", "prio"};
-
-    private static final long ITERATIONS = 30;
-
-    private static String fileIn, anonMode, buildMode;
-    private static int k;
-    private static boolean printAnonGraph;
+    private static String[] ANON_ALGOS      = new String[]{"greedy", "dp"};
+    private static String[] CONSTR_ALGOS    = new String[]{"constr", "prio"};
 
     public static void main(String[] args) throws InterruptedException, IOException {
-        parseArgs(args);
+        String fileIn       = args[0];
+        int k               = Integer.parseInt(args[1]);
+        String anonMode     = args[2];
+        String buildMode    = args[3];
+        boolean printAnonGraph = Boolean.parseBoolean(args[4]);
+        boolean feasible;
 
         ArrayList<ArrayList<String>> graph = LoadData.readData(fileIn);
+
         ArrayList<Degree> dist = LoadData.getDegrees(graph);
         ArrayList<Edge> origGraph = LoadData.getEdges(graph);
 
@@ -28,34 +29,22 @@ public class Main {
         ArrayList<DA> das = new ArrayList<>();
         ArrayList<Degree> grouped = null;
         ArrayList<Edge> edges = null;
+        
+        // Inizio a misurare il tempo
+        long startTime = System.currentTimeMillis();
 
-        int  groupCost = 0;
-        long groupTime = 0;
+        int anonCost = 0;
         if (anonMode.equals(ANON_ALGOS[0])) {
             // Creazione lista dei gradi con algoritmo: greedy
-            for (int i = 0; i < ITERATIONS; i++) {
-                long startTime = System.nanoTime();
-
-                Grouper.greedyGrouper_1(k, dist);
-                groupTime += (System.nanoTime() - startTime)/ITERATIONS;
-            }
-            grouped = Grouper.greedyGrouper_1(k, dist);
-            groupCost = computeCostGreedy(dist, grouped);
+            grouped = Grouper.greedyGrouper(k, dist);
+            anonCost = computeCostGreedy(dist, grouped);
         }
         else if (anonMode.equals(ANON_ALGOS[1])) {
             // Creazione lista dei gradi con algoritmo: dynamic programming
-            for (int i = 0; i < ITERATIONS; i++) {
-                das = new ArrayList<>();
-                long startTime = System.nanoTime();
-                Grouper.dpGrouper(k, dist, das);
-                groupTime += (System.nanoTime() - startTime)/ITERATIONS;
-            }
-            das = new ArrayList<>();
             grouped = Grouper.dpGrouper(k, dist, das);
-            groupCost = computeCostDP(das);
+            anonCost = computeCostDP(das);
         }
-<<<<<<< HEAD
-/*
+
         StringBuilder out = new StringBuilder();
         out.append("[");
         for (Degree d : grouped) {
@@ -66,55 +55,36 @@ public class Main {
         out.deleteCharAt(out.length()-1);
         out.append("]");
 
-        System.out.println(out.toString());
-*/
+        //System.out.println(out.toString());
 
 
 
-        // Inizio a misurare il tempo
-        long startTime = System.currentTimeMillis();
 
-=======
+        
+        
 
-        //functionTest(grouped, k);
->>>>>>> ec7bbd2d699f87ab6067e3f5de27878637f9fcf3
 
-       // Costruisco il grafo anonimizzato
-        long constructTime = 0;
         if (buildMode.equals(CONSTR_ALGOS[0])) {
             //Anonimizza con l'algoritmo greedy
             edges = Construct.greedyConstructGraph(grouped);
-            for (int  i = 0; i < ITERATIONS; i++) {
-                long startTime = System.nanoTime();
-                Construct.greedyConstructGraph(grouped);
-                constructTime += (System.nanoTime() - startTime)/ITERATIONS;
-            }
-
         }
         else if (buildMode.equals(CONSTR_ALGOS[1])) {
-        //else if (buildMode.equals("prio")) {
             //Anonimizza con l'algoritmo Priority
             edges = Construct.priorityConstructGraph(grouped, origGraph);
-            for (int i = 0; i < ITERATIONS; i++) {
-                long startTime = System.nanoTime();
-                Construct.priorityConstructGraph(grouped, origGraph);
-                constructTime += (System.nanoTime() - startTime)/ITERATIONS;
-            }
         }
 
-
+        long finishTime = System.currentTimeMillis();
         // Misuro il tempo impiegato a costruire il grafo anonimizzato
-        long totalTime  = groupTime + constructTime;
-
-        boolean feasible = (!edges.isEmpty());
+        long totalTime  = finishTime - startTime;
 
         if (printAnonGraph) {
             // Stampo il nuovo grafo anonimizzato
             printAnonymizedGraph(edges, k, fileIn);
         }
-
+        
+        feasible = !edges.isEmpty();
         // Stampo i risultati
-        printCostToCSV(fileIn, k, anonMode, buildMode, groupCost, totalTime/1000, feasible);
+        printCostToCSV(fileIn, k, anonMode, buildMode, anonCost, totalTime, feasible);
     }
 
     private static int computeCostGreedy(ArrayList<Degree> oldDegrees, ArrayList<Degree> groupedDegrees) {
@@ -147,7 +117,7 @@ public class Main {
 
 
     private static void printCostToCSV(String fileIn, int k, String anonMode, String buildMode, int cost, long time, boolean feasible) throws IOException {
-        FileWriter fw   = new FileWriter("output.csv", true);
+        FileWriter fw   = new FileWriter("..//output//output_" + fileIn + ".csv", true);
         PrintWriter pw  = new PrintWriter(fw);
         String output   = fileIn + ";" + k + ";" + anonMode + ";" + buildMode + ";" + cost + ";" + time + ";" + feasible;
 
@@ -182,42 +152,5 @@ public class Main {
             pw.println();
         }
         pw.close();
-    }
-
-
-    private static void functionTest(ArrayList<Degree> grouped, int k) {
-        ArrayList<Integer> deg = new ArrayList<>();
-        for (Degree d : grouped) {
-            deg.add(d.getDegree());
-        }
-
-        for (Integer i : deg) {
-            int freq = Collections.frequency(deg, i);
-            if (freq < k) {
-                System.out.println("ERRORE frequenza: " + i + " freq: " + freq);
-                break;
-            }
-            //System.out.print("[ elem: " + i + " freq: " + freq + " ], ");
-        }
-
-        StringBuilder out = new StringBuilder();
-        out.append("[");
-        for (Degree d : grouped) {
-            out.append(d.getDegree());
-            out.append(", ");
-        }
-        out.deleteCharAt(out.length()-1);
-        out.deleteCharAt(out.length()-1);
-        out.append("]");
-
-        System.out.println(out.toString());
-    }
-
-    private static void parseArgs(String[] args) {
-        fileIn = args[0];
-        k = Integer.parseInt(args[1]);
-        anonMode = args[2];
-        buildMode = args[3];
-        printAnonGraph = Boolean.parseBoolean(args[4]);
     }
 }
